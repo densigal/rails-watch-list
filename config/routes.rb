0687d578c926d85e2ec75d12fe85bde595ef9fe9
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-14 16:58:34 +0900

    started bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'lists', to: 'lists#index', as: :lists
+  get 'lists/:id', to: 'lists#show', as: :list
 
+  get 'lists/new', to: 'lists#new', as: :new_list
+  post 'lists', to: 'lists#create'
+
+  get 'lists/:list_id/bookmark/new', to: 'bookmarks#new', as: :new_list_bookmark
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
